81cb933eeeb6a9d21dd777f477b70edbd59cccd3
@trevorgriest <tgriest1981@gmail.com>
2019-09-04 23:30:42 -0400

    First Commit

---
+++ integersEqual/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integersEqual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "This Program Determines Whether or Not Two Integers are Equal");
+
+            Console.WriteLine("Please Enter Your First Integer");
+
+            string firstInt = Console.ReadLine();
+
+            Console.WriteLine("You Entered " + firstInt + ".");
+
+            Console.WriteLine("Please Enter Your Second Integer");
+
+            string secondInt = Console.ReadLine();
+
+            Console.WriteLine("You Entered " + secondInt + ".");
+
+            if (firstInt == secondInt)
+            {
+                Console.WriteLine("Your Integers are Equal!");
+            }
+            else {
+                Console.WriteLine("Your Integers are Not Equal!");
+            }
+            
+        }
+    }
+}
